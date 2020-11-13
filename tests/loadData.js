948212c7f3775aef257a1c0e3a87bce59207c843
amal-san <amaldemaniax@gmail.com>
2020-11-13 19:45:59 +0530

    load data for users

--- tests/loadData.js
+++ tests/loadData.js
@@ -13,31 +13,10 @@ describe('Loading 50 user data to database..', () => {
         chai
         .request(server)
         .post('/user/create')
-        .send({'username':faker.internet.userName() , 'password':faker.internet.password(),'first_name':faker.name.firstName(),'last_name':faker.name.lastName(),'email':faker.internet.email()})
+        .send({'username':faker.internet.userName() ,'password':faker.internet.password(),'first_name':faker.name.firstName(),'last_name':faker.name.lastName(),'email':faker.internet.email()})
         .end((err, res) => {
-        res.should.have.status(200);
-        (res.body).should.be.a('object');
-        })}
-        done();
-    });
-});
-
-describe('Loading 50 books data to database..', () => {
-    it('It load 50 user data', (done) => {
-      for(var i=1;i<50;i++){
-        chai
-        .request(server)
-        .put('/book/create')
-        .send({  'name':faker.name.findName() , 
-                 'author':faker.name.findName(),
-                 'price':faker.finance.amount(),
-                 'description':faker.lorem.sentence(),
-                 'genre':faker.company.catchPhrase(),
-            })
-
-        .end((err, res) => {
-        res.should.have.status(200);
-        (res.body).should.be.a('object');
+            res.should.have.status(200);
+            (res.body).should.be.a('object');
         })}
         done();
         setTimeout((function() {
@@ -45,3 +24,27 @@ describe('Loading 50 books data to database..', () => {
         }), 1500);
     });
 });
+
+// describe('Loading 50 books data to database..', () => {
+//     it('It load 50 user data', (done) => {
+//       for(var i=1;i<50;i++){
+//         chai
+//         .request(server)
+//         .put('/book/create')
+//         .send({  'name':faker.name.findName() , 
+//                  'author':faker.name.findName(),
+//                  'price':faker.finance.amount(),
+//                  'description':faker.lorem.sentence(),
+//                  'genre':faker.company.catchPhrase(),
+//             })
+
+//         .end((err, res) => {
+//         res.should.have.status(200);
+//         (res.body).should.be.a('object');
+//         })}
+//         done();
+//         setTimeout((function() {
+//             return process.exit()
+//         }), 1500);
+//     });
+// });
